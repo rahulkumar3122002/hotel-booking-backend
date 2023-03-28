19a7f77f9d313b2efbde6d7ab8d002fb1812a84f
Rahul Bharodiya <rbkumar01234@gmail.com>
2023-03-28 13:55:49 +0530

    some changes

--- model/schema.js
+++ model/schema.js
@@ -8,8 +8,8 @@ const dataSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      required: true,
       unique: true,
+      required: true,
     },
     password: {
       type: String,
